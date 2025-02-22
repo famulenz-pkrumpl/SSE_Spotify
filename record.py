bd503c86ac95a885206b32654df7ba9716e42763
Julian Hirschler <julian.j.hirschler@gmail.com>
2025-02-22 17:03:15 +0100

    add env variables to record

--- record.py
+++ record.py
@@ -1,15 +1,13 @@
 import subprocess
+import os
+from dotenv import load_dotenv
 
-# Absolute path to energibridge executable
-energibridge_path = r""
-# Absolute path to monitoring service
-driver_path = r""
+load_dotenv()
 
-# Service name
-service_name = "rapl"
-# Absolute path to result directory - path should end with backslash
-csv_output_dir_path = r""
-timeout_in_seconds = 5
+ENERGIBRIDGE_PATH = os.getenv("ENERGIBRIDGE_PATH")
+DRIVER_PATH = os.getenv("DRIVER_PATH")
+SERVICE_NAME = os.getenv("SERVICE_NAME")
+CSV_OUTPUT_DIR_PATH = os.getenv("CSV_OUTPUT_DIR_PATH")
 
 
 def run_admin_command(command):
@@ -28,32 +26,34 @@ def service_exists(service):
 
 
 def delete_service():
-    if service_exists(service_name):
-        print(f"Service '{service_name}' will be deleted now...")
-        run_admin_command(f'sc delete {service_name}')
+    if service_exists(SERVICE_NAME):
+        print(f"Service '{SERVICE_NAME}' will be deleted now...")
+        run_admin_command(f'sc delete {SERVICE_NAME}')
 
 
 def setup_service():
     delete_service()
 
-    print(f"Creating Service '{service_name}'...")
-    run_admin_command(f'sc create {service_name} type=kernel binPath=\"{driver_path}\"')
+    print(f"Creating Service '{SERVICE_NAME}'...")
+    run_admin_command(f'sc create {SERVICE_NAME} type=kernel binPath=\"{DRIVER_PATH}\"')
 
-    print(f"Starting Service '{service_name}'...")
-    run_admin_command(f'sc start {service_name}')
+    print(f"Starting Service '{SERVICE_NAME}'...")
+    run_admin_command(f'sc start {SERVICE_NAME}')
 
 
-def run_energibridge(output_file_name):
+def run_energibridge(output_file_name, timeout_in_seconds):
     print("Starting EnergiBridge...")
-    result = subprocess.run([energibridge_path, "-o", csv_output_dir_path + output_file_name, "--summary", "timeout", str(timeout_in_seconds)], shell=True)
+    result = subprocess.Popen([ENERGIBRIDGE_PATH, "-o", CSV_OUTPUT_DIR_PATH + output_file_name, "--summary", "timeout", str(timeout_in_seconds)], shell=True)
 
     if result.returncode == 0:
-        print(f"Finished EnergiBridge execution, results are in: '{csv_output_dir_path}'")
+        print(f"Finished EnergiBridge execution, results are in: '{CSV_OUTPUT_DIR_PATH}'")
     else:
         print("Error executing EnergiBridge")
+
+    return result
 
 
 if __name__ == "__main__":
     setup_service()
-    run_energibridge("test1.csv")
+    run_energibridge("test1.csv", 5)
     delete_service()
