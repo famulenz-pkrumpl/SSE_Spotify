b7b33bc3da79e4d0f08663853060f9fdb6f0870d
Julian Hirschler <julian.j.hirschler@gmail.com>
2025-02-22 17:03:29 +0100

    add energibridge to main

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
+import os
 import time
-import spotify
 
-# Hndles for spotify web an native windows
+import record
+import spotify
+import screen_brightness_control as sbc
+
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from comtypes import CLSCTX_ALL
+from dotenv import load_dotenv
+
+load_dotenv()
+
+VOLUME = float(os.getenv("VOLUME", 0.2))
+BRIGHTNESS = int(os.getenv("BRIGHTNESS", 50))
+
+
+# Handles for spotify web a native windows
 spotify_web = None
 spotify_native = None
+
+
+def unmute_and_set_volume(level):
+  devices = AudioUtilities.GetSpeakers()
+  interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+  volume = interface.QueryInterface(IAudioEndpointVolume)
+
+  if volume.GetMute():
+    volume.SetMute(0, None)
+
+  volume.SetMasterVolumeLevelScalar(level, None)
+
+
+def set_brightness(level):
+  sbc.set_brightness(level)
+
 
 # Initialize the experiment
 def init():
   # TODO: maybe adapt any energy settings for the PC automatically
+
+  # Set volume and brightness of the system
+  unmute_and_set_volume(VOLUME)
+  set_brightness(BRIGHTNESS)
 
   # Open Spotify native
   global spotify_native
@@ -19,7 +53,8 @@ def init():
   spotify_web = spotify.open_web()
   time.sleep(5)
 
-  # TODO: initialize EnergiBridge
+  # TODO: initialize EnergiBridge  # Create monitoring service
+  record.setup_service()
 
 # Simple implementation of the fibonacci sequence used for warm up
 def fibonacci(n):
@@ -50,19 +85,33 @@ def warm_up():
 # Run the experiment
 def run_experiment():
   # TODO: implement experiment run
-  # Focus the native Spotify app
-  spotify.focus(spotify_native)
-  time.sleep(1)
-  spotify.play_song("Never gonna give you up")
-  time.sleep(10)
-  spotify.pause_song()
-  time.sleep(1)
+
+  songs = [
+    "Never Gonna Give You Up",
+    "Take On Me"
+  ]
+
+  for i, song in enumerate(songs):
+    output_file_name = f"experiment_{i}.csv"
+
+    print(f"Running experiment for song: {song}")
+    process = record.run_energibridge(output_file_name, 20)
+
+    # Focus the native Spotify app
+    spotify.focus(spotify_native)
+    time.sleep(1)
+    spotify.play_song(song)
+    time.sleep(10)
+    spotify.pause_song()
+    time.sleep(1)
+
+    process.wait()
   
   return
 
 
 # Start the procedure
-warm_up()
+#warm_up()
 init()
 run_experiment()
 
