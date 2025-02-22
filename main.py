f39737497a6571eaf6f689d3967b744362cf1835
Patrick Krumpl <patrick.krumpl@student.tugraz.at>
2025-02-22 15:31:44 +0100

    main structure

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
 import time
 import spotify
 
-# Open Spotify app
-spotify_native = spotify.open_native()
-time.sleep(5)
+# Hndles for spotify web an native windows
+spotify_web = None
+spotify_native = None
 
-# Open Spotify web
-spotify_web = spotify.open_web()
-time.sleep(5)
+# Initialize the experiment
+def init():
+  # TODO: maybe adapt any energy settings for the PC automatically
 
-# Focus the native Spotify app
-spotify.focus(spotify_native)
-time.sleep(1)
-spotify.play_song("Shape of You")
-time.sleep(10)
-spotify.pause_song()
-time.sleep(1)
+  # Open Spotify native
+  global spotify_native
+  spotify_native = spotify.open_native()
+  time.sleep(5)
 
-# Focus the web Spotify app
-spotify.focus(spotify_web)
-time.sleep(1)
-spotify.play_song("Never gonna give you up")
-time.sleep(10)
-spotify.pause_song()
-time.sleep(1)
+  # Open Spotify web
+  global spotify_web
+  spotify_web = spotify.open_web()
+  time.sleep(5)
+
+  # TODO: initialize EnergiBridge
+
+# Simple implementation of the fibonacci sequence used for warm up
+def fibonacci(n):
+  if n <= 1:
+    return n
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
+
+# Do some warm up before actually running the experiment
+# The warm up is done by running a CPU intensive task (fibonacci)
+def warm_up():
+  print("Start warm up")
+  # Warm up for 1 minute
+  warm_up_time = 60
+  start_time = time.time()
+
+  i = 1
+  while time.time() - start_time < warm_up_time:
+    print(fibonacci(i), " ")
+    i = i + 1
+
+  print("Warm up completed")
+  time.sleep(1)
+
+  return
+
+
+# Run the experiment
+def run_experiment():
+  # TODO: implement experiment run
+  # Focus the native Spotify app
+  spotify.focus(spotify_native)
+  time.sleep(1)
+  spotify.play_song("Never gonna give you up")
+  time.sleep(10)
+  spotify.pause_song()
+  time.sleep(1)
+  
+  return
+
+
+# Start the procedure
+warm_up()
+init()
+run_experiment()
+
